e2ee94b27997969c24084b54884d78f95939f1f9
Andrey Ryabtsev <aryabtsev1@gmail.com>
2019-07-26 21:24:55 -0700

    isNaN() instead of == NaN

--- bot.js
+++ bot.js
@@ -388,14 +388,13 @@ commands["f"] = (message) => {
 
 commands["flipcoin"] = (message, text) => {
     let numCoins = parseInt(util.args(text)[0]);
-    if (numCoins == NaN || numCoins < 1) numCoins = 1;
+    if (isNaN(numCoins) || numCoins < 1) numCoins = 1;
     if (numCoins > config["etc"]["max_coins"]) {
         message.channel.send(config["etc"]["coin_error"]);
         return;
     }
     let heads = 0;
     for (let i = 0; i < numCoins; i++) if (Math.random() > 0.5) heads++;
-    console.error(numCoins, heads);
     if (numCoins == 1) message.channel.send(heads == 1 ? "Heads!" : "Tails!");
     else message.channel.send(config["etc"]["coin_output_many"].replace("{n}", heads));
 }
@@ -510,7 +509,7 @@ commands["quote"] = (message, text) => {
 
 commands["rng"] = (message, text) => {
     let max = parseInt(util.args(text)[0]);
-    if (max == NaN || max <= 0) {
+    if (isNaN(max) || max <= 0) {
         message.channel.send(config["etc"]["rng_error"]);
         return;
     }
