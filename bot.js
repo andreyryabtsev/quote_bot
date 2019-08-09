f9c9c70d9628bdfac21852d4f033f4acb304c779
Max Randal <53409307+randalpik@users.noreply.github.com>
2019-08-09 07:20:48 +0100

    added the actual feature i was trying to implement (#6)
    * added the actual feature i was trying to implement
    
    * fixed callback

--- bot.js
+++ bot.js
@@ -226,6 +226,7 @@ let buildQuoteGlossary = (quotes) => {
 }
 let sendQuote = (channel, content, author) => {
     channel.send(config["quotes"]["format"].replace("{q}", content).replace("{u}", author));
+    db.updateQuote(content, Date.now());
 }
 
 // CFG: Compute and store the vocab lists from database, refreshing with a new query when over
