eee81fea29dee430ca21b36449072a6ba7c52bb3
Andrey Ryabtsev <ryabtsev@cs.washington.edu>
2019-08-13 21:30:26 -0700

    update bot.js to master

--- bot.js
+++ bot.js
@@ -597,21 +597,22 @@ commands["remindme"] = (message, text) => {
     let seconds = util.timeToSecs(util.args(text)[0]),
         note = text.substring(text.indexOf(" ") + 1),
         now = Date.now();
-    if (!seconds === null) {
+    if (note === text) note = "";
+    if (seconds === null) {
         message.channel.send(config["reminders"]["format_error"]);
-        return;
-    }
-    db.addReminder(message.author.id, message.channel.id, now, note, seconds, (results) => {
-        reminders.push({
-            id: results.insertId,
-            channelID: message.channel.id,
-            discordID: message.author.id,
-            start: now,
-            seconds: seconds,
-            note: note
+    } else {
+        db.addReminder(message.author.id, message.channel.id, now, note, seconds, (results) => {
+            reminders.push({
+                id: results.insertId,
+                channelID: message.channel.id,
+                discordID: message.author.id,
+                start: now,
+                seconds: seconds,
+                note: note
+            });
+            message.react(ACKNOWLEDGEMENT_EMOTE);
         });
-        message.react(ACKNOWLEDGEMENT_EMOTE);
-    });
+    }
 }
 
 commands["rng"] = (message, text) => {
