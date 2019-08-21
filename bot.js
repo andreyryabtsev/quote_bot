a098c56b0e837dcd093b5f53de337e9da3755f9e
Andrey Ryabtsev <aryabtsev1@gmail.com>
2019-08-20 20:25:41 -0700

    implement numquotesby

--- bot.js
+++ bot.js
@@ -501,6 +501,17 @@ commands["numquotes"] = (message, text) => {
     });
 }
 
+commands["numquotesby"] = (message, text) => {
+    let users = message.mentions.users.map(u => u.id);
+    if (!users) {
+        message.channel.send(config["quotes"]["num_author_error"]);
+        return;
+    }
+    db.filteredUserQuotes(users, quotes => {
+        message.channel.send(config["quotes"]["num_quotes"].replace("{n}", quotes.length));
+    });
+}
+
 commands["ping"] = (message) => {
     message.channel.send(config["general"]["ping_response"]);
 }
