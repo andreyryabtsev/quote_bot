0f64ff9d2ac7d79a14c20038ed221d3bc7516702
Andrey Ryabtsev <aryabtsev1@gmail.com>
2019-09-11 14:06:45 -0700

    complete basic refactor

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,4 +1,4 @@
-commands["help"] = (message) => {
+module.exports = (message) => {
     let help = util.splitLongMessage(generateHelp());
     for (let msg of help) message.channel.send(msg);
 }
--- src/commands/vote.js
+++ src/commands/vote.js
@@ -25,3 +25,31 @@ module.exports = (message, text) => {
         addVoteReactions(voteMessage, 0, options.length);
     });
 }
+
+// Recursively iterate over 0..n-1 and add vote reactions sequentially.
+let addVoteReactions = (message, i, n) => {
+    if (i == n) return;
+    message.react(VOTE_REACTIONS[i]).then(r => addVoteReactions(message, i + 1, n));
+}
+
+// Parse the reactions to a vote and the vote object, constructing a text summary.
+let parseVoteMessage = (message, voteInfo) => {
+    let longestOption = 0, totalVotes = 0;
+    let votes = [];
+    message.reactions.forEach(reaction => {
+        let optionIndex = VOTE_REACTIONS.indexOf(reaction.emoji.name);
+        if (optionIndex > -1 && optionIndex < voteInfo.options.length) {
+            totalVotes += reaction.count - 1;
+            let label = voteInfo.options[optionIndex];
+            if (label.length > longestOption) longestOption = label.length;
+            votes.push({label: label, count: reaction.count - 1});
+        }
+    });
+    let output = "```\n" + voteInfo.content + ":\n";
+    for (let vote of votes) {
+        let percentage = totalVotes == 0 ? "0.00" : (vote.count / totalVotes * 100).toFixed(2);
+        output += vote.label + " ".repeat(longestOption - vote.label.length) + ": " + vote.count + " (" + percentage + "%)\n";
+    }
+    output += "```";
+    return output;
+}
