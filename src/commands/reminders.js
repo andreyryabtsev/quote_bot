31c4075c38631039d48ee4e606f13d9ce2dbb609
Andrey Ryabtsev <aryabtsev1@gmail.com>
2019-09-11 18:37:15 -0700

    add reminders

--- src/commands/reminders.js
+++ src/commands/reminders.js
@@ -1,21 +1,25 @@
-module.exports = (core, message, text) => {
-    let seconds = core.util.timeToSecs(core.util.args(text)[0]),
-        note = text.substring(text.indexOf(" ") + 1),
-        now = Date.now();
-    if (note === text) note = "";
-    if (seconds === null) {
-        message.channel.send(core.config["reminders"]["format_error"]);
-    } else {
-        core.db.addReminder(message.author.id, message.channel.id, now, note, seconds, (results) => {
-            reminders.push({
-                id: results.insertId,
-                channelID: message.channel.id,
-                discordID: message.author.id,
-                start: now,
-                seconds: seconds,
-                note: note
-            });
-            message.react(core.ACKNOWLEDGEMENT_EMOTE);
-        });
-    }
+module.exports = (core, message) => {
+    let discordID = message.author.id;
+    db.allReminders((reminders) => {
+        if (reminders.length == 0) {
+            message.channel.send(config["reminders"]["output_empty"]);
+        } else {
+            let output = "```";
+            let title = config["reminders"]["output_title"];
+            output += title ? title + "\n" : "";
+            for (let reminder of reminders) {
+                let alarmTime = parseInt(reminder.invoked_on) + reminder.delay_seconds * 1000;
+                let duration = util.formatDuration(alarmTime - Date.now());
+                if (reminder.discord_id == discordID) { // print only the reminders belonging to caller
+                    output += config["reminders"]["output_row"]
+                        .replace("{d}", duration)
+                        .replace("{n}", reminder.content)
+                        + "\n";
+                }
+            }
+            let footer = config["reminders"]["output_footer"];
+            output += footer ? footer + "```" : "```";
+            message.channel.send(output);
+        }
+    });
 }
