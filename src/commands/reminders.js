233142f5ef74559972cb6bcdbecb7854f64cac3e
Andrey Ryabtsev <aryabtsev1@gmail.com>
2019-09-12 11:06:00 -0700

    remove logging, fix reminder printout

--- src/commands/reminders.js
+++ src/commands/reminders.js
@@ -4,20 +4,19 @@ module.exports = (core, message) => {
         if (reminders.length == 0) {
             message.channel.send(core.config["reminders"]["output_empty"]);
         } else {
+            let callerReminders = reminders.filter(r => r.discord_id == discordID);
             let output = "```";
             let title = core.config["reminders"]["output_title"];
             output += title ? 
-                title.replace("{n}", reminders.length).replace("{p}", reminders.length > 0 ? "s" : "")
-                 + "\n" : "";
-            for (let reminder of reminders) {
+                title.replace("{n}", callerReminders.length)
+                .replace("{p}", callerReminders.length > 0 ? "s" : "") + "\n" : "";
+            for (let reminder of callerReminders) {
                 let alarmTime = parseInt(reminder.invoked_on) + reminder.delay_seconds * 1000;
                 let duration = core.util.formatDuration(alarmTime - Date.now());
-                if (reminder.discord_id == discordID) { // print only the reminders belonging to caller
-                    output += core.config["reminders"]["output_row"]
-                        .replace("{d}", duration)
-                        .replace("{n}", reminder.content)
-                        + "\n";
-                }
+                output += core.config["reminders"]["output_row"]
+                    .replace("{d}", duration)
+                    .replace("{n}", reminder.content)
+                    + "\n";
             }
             let footer = core.config["reminders"]["output_footer"];
             output += footer ? footer + "```" : "```";
