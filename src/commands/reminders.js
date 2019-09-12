05c024c065ad370ef375c6218ca7de3d449bb5a4
Andrey Ryabtsev <aryabtsev1@gmail.com>
2019-09-11 19:00:47 -0700

    fix reminders to honor new system

--- src/commands/reminders.js
+++ src/commands/reminders.js
@@ -1,23 +1,23 @@
 module.exports = (core, message) => {
     let discordID = message.author.id;
-    db.allReminders((reminders) => {
+    core.db.allReminders((reminders) => {
         if (reminders.length == 0) {
-            message.channel.send(config["reminders"]["output_empty"]);
+            message.channel.send(core.config["reminders"]["output_empty"]);
         } else {
             let output = "```";
-            let title = config["reminders"]["output_title"];
+            let title = core.config["reminders"]["output_title"];
             output += title ? title + "\n" : "";
             for (let reminder of reminders) {
                 let alarmTime = parseInt(reminder.invoked_on) + reminder.delay_seconds * 1000;
-                let duration = util.formatDuration(alarmTime - Date.now());
+                let duration = core.util.formatDuration(alarmTime - Date.now());
                 if (reminder.discord_id == discordID) { // print only the reminders belonging to caller
-                    output += config["reminders"]["output_row"]
+                    output += core.config["reminders"]["output_row"]
                         .replace("{d}", duration)
                         .replace("{n}", reminder.content)
                         + "\n";
                 }
             }
-            let footer = config["reminders"]["output_footer"];
+            let footer = core.config["reminders"]["output_footer"];
             output += footer ? footer + "```" : "```";
             message.channel.send(output);
         }
