60d58f3b75f434cb137e67838370f469b09090c9
Andrey Ryabtsev <aryabtsev1@gmail.com>
2019-09-11 18:58:46 -0700

    Readd reminders and choose questions (#12)
    * add reminders
    
    * add and trim

--- src/shared/nlp.js
+++ src/shared/nlp.js
@@ -1,10 +1,32 @@
+const REPLACEMENT_MAP = {
+    " my ":" your ", " your ":" my ",
+    " myself ":" yourself ", " yourself ":" myself ",
+    " you ":" me ", " me ":" you "
+};
+const MARKERS = [": ", "choose between ", "choose from ", "choose ", "would you rather "];
+
+let markerRegex = new RegExp(MARKERS.join("|"), "g"),
+    replacementRegex = new RegExp(Object.keys(REPLACEMENT_MAP).join("|"), "g");
+
 module.exports = (core, message) => {
     let config = core.config, util = core.util, text = message.content.toLowerCase();
-    if ((text.startsWith("bot") || text.startsWith(config["general"]["bot_name"])) && text.endsWith("?")) {
-        if (text.includes("percent") || text.includes("%")) {
-            message.channel.send(Math.floor(Math.random() * 101) + "%!");
-        } else {
-            message.channel.send((Math.random() < 0.5) ? "Yes!" : "No!");
+    if (text.startsWith("bot") || text.startsWith(config["general"]["bot_name"])) {
+        if (markerRegex.test(text)) { // continues if the input contains one of the keywords
+            text = text.replace(/(, or | or )/g, ", ");
+            text.replace(markerRegex, "");
+            let choices = text.split(", ").map(word => {
+                if (word.endsWith("?") || word.endsWith(".")) word = word.slice(0, word.length - 1);
+                word = " " + word + " ";
+                return word.replace(replacementRegex, match => replacements[match.toLowerCase()]).trim();
+            });
+            let output = core.util.simpleRandom(choices);
+            message.channel.send(output.charAt(0).toUpperCase() + output.slice(1) + "!");
+        } else if (text.endsWith("?")) {
+            if (text.includes("percent") || text.includes("%")) {
+                message.channel.send(Math.floor(Math.random() * 101) + "%!");
+            } else {
+                message.channel.send((Math.random() < 0.5) ? "Yes!" : "No!");
+            }
         }
     } else if (text.includes(config["general"]["bot_name"])) {
         hearts = ["💚", "💜", "🖤", "💛", "💙", "❤️"];
