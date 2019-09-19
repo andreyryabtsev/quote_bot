3e26b544bf198f5e53d329a27eec641f747867e6
Andrey Ryabtsev <aryabtsev1@gmail.com>
2019-09-19 15:41:48 -0700

    sort reminders

--- src/commands/reminders.js
+++ src/commands/reminders.js
@@ -4,15 +4,20 @@ module.exports = (core, message) => {
         if (reminders.length == 0) {
             message.channel.send(core.config["reminders"]["output_empty"]);
         } else {
-            let callerReminders = reminders.filter(r => r.discord_id == discordID);
+            let callerReminders = reminders.filter(r => r.discord_id == discordID)
+                .map(reminder => {
+                    let alarmTime = parseInt(reminder.invoked_on) + reminder.delay_seconds * 1000;
+                    reminder.duration = alarmTime - Date.now();
+                    return reminder;
+                })
+                .sort((r1, r2) => r1.duration - r2.duration);
             let output = "```";
             let title = core.config["reminders"]["output_title"];
             output += title ? 
                 title.replace("{n}", callerReminders.length)
                 .replace("{p}", callerReminders.length > 0 ? "s" : "") + "\n" : "";
             for (let reminder of callerReminders) {
-                let alarmTime = parseInt(reminder.invoked_on) + reminder.delay_seconds * 1000;
-                let duration = core.util.formatDuration(alarmTime - Date.now());
+                let duration = core.util.formatDuration(reminder.duration);
                 output += core.config["reminders"]["output_row"]
                     .replace("{d}", duration)
                     .replace("{n}", reminder.content)
