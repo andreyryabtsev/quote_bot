a1ca2c3aac395106a66380d57bc530a645566be3
Andrey Ryabtsev <aryabtsev1@gmail.com>
2020-06-20 18:25:04 -0700

    Update logs.js

--- src/commands/logs.js
+++ src/commands/logs.js
@@ -4,7 +4,7 @@ module.exports = (core, message, text) => {
             : message.author.id;
     let args = core.util.args(text);
     let count = args.length >= 1 ? parseInt(args[0]) : 5;
-    core.db.recentLogs(id, count results => {
+    core.db.recentLogs(id, count, results => {
         let result_lines = [];
         for (let row of results) {
             let time_since = Date.now() - row.created_at, caption = row.content;
